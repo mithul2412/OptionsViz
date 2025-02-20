33da0a215c22257e259bdfb9caac99d6999d14f8
Ryan Richards <ryan@m4.local>
2025-02-20 09:45:51 -0800

    retry init commit

---
+++ scripts/app.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+for k, v in st.session_state.items():
+    st.session_state[k] = v
+
+st.set_page_config(
+    page_title="Options Data Visualization",
+    page_icon="💰",
+    layout="wide",
+    initial_sidebar_state="expanded",
+
+)
+
+pg = st.navigation([st.Page("single_chain.py", title='Single Chain'),
+                    st.Page("dynamic_chain.py", title='Dynamic Chain')])
+pg.run()
