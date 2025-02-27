ff361d22212bd9eb4161bc58bab6b3a4e7c15d38
Ryan Richards <ryan@m4.local>
2025-02-27 14:10:15 -0800

    added basic build files and simple test

---
+++ scripts/tests/test_utils.py
@@ -0,0 +1,12 @@
+import unittest
+import numpy as np
+from scripts.utils import chain_utils
+
+class TestUtils(unittest.TestCase):
+
+    def test_add(self):
+        res = chain_utils.add(1,2)
+        self.assertAlmostEqual(res, 3, delta=1e-08, msg="Probabilities do not egaul the negative log of the base probability")
+
+if __name__ == '__main__':
+    unittest.main()
