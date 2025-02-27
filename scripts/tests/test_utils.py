fd000ded981def80ccc573a3ee746fae56d8ff24
Ryan Richards <ryan@m4.local>
2025-02-27 15:04:07 -0800

    modded function name

--- scripts/tests/test_utils.py
+++ scripts/tests/test_utils.py
@@ -5,8 +5,8 @@ from utils import chain_utils
 class TestUtils(unittest.TestCase):
 
     def test_add(self):
-        res = chain_utils.add(1,2)
-        self.assertAlmostEqual(res, 3, delta=1e-08, msg="Probabilities do not egaul the negative log of the base probability")
+        res = chain_utils.add_nums(1,2)
+        self.assertAlmostEqual(res, 3, delta=1e-08, msg="numbers are not equal")
 
 if __name__ == '__main__':
     unittest.main()
